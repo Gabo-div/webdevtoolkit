25ff8d4f0f1f958155002d6309a678f7158d004c
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2025-04-06 22:00:53 -0400

    feat: metadata and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wdtoolkit.vercel.app/",
   redirects: {
     "/": "/tools",
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
+
